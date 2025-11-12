df2e645a5e805bc41890f2791610ce543bb818f1
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-11-12 14:18:43 +0100

    first iteration with status controller to test infra

---
+++ src/main/java/com/yzlaboratory/api_catalog/ApiCatalog.java
@@ -0,0 +1,13 @@
+package com.yzlaboratory.api_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCatalog {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCatalog.class, args);
+	}
+
+}
---
+++ src/main/java/com/yzlaboratory/api_catalog/controller/StatusController.java
@@ -0,0 +1,16 @@
+package com.yzlaboratory.api_catalog;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public String status() {
+        return "<h1>Hello World, its me the Status Controller of your friend api-catalog</h1>";
+    }
+}
