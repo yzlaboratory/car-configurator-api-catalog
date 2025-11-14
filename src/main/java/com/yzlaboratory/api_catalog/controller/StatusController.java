ba5f4531f6f8149f02761473bec255f2b17fc080
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-11-14 19:17:57 +0100

    log on status controller hit

--- src/main/java/com/yzlaboratory/api_catalog/controller/StatusController.java
+++ src/main/java/com/yzlaboratory/api_catalog/controller/StatusController.java
@@ -11,6 +11,7 @@ public class StatusController {
 
     @GetMapping("/status")
     public String status() {
+        System.out.println("Status Controller called");
         return "<h1>Hello World, its me the Status Controller of your friend api-catalog</h1>";
     }
 }
