8c9054dab965613328be2cb7459c55bfc2e136f8
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-11-16 09:23:25 +0100

    changed path to /catalog/status

--- src/main/java/com/yzlaboratory/api_catalog/controller/StatusController.java
+++ src/main/java/com/yzlaboratory/api_catalog/controller/StatusController.java
@@ -3,10 +3,13 @@ package com.yzlaboratory.api_catalog.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/catalog")
 public class StatusController {
 
     @GetMapping("/status")
